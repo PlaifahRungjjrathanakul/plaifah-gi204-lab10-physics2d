0509a8415686774e3dd48bd915847b056c9b405e
PlaifahRungjjrathanakul <plaifah.rung@bumail.net>
2025-04-21 14:54:02 +0700

    UpdateCameraFollow2D

--- Assets/Script/CameraFollow2D.cs
+++ Assets/Script/CameraFollow2D.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class CameraFollow2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
 
-    // Update is called once per frame
-    void Update()
+    
+    private float smoothTime = 0.25f;
+
+    
+    private Vector3 velocity = Vector3.zero;
+
+    
+    [SerializeField] private Transform target;
+
+    
+    private void Update()
     {
         
+        Vector3 targetPosition = target.position + offset;
+
+        
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
